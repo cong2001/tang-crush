8ec144bf0640b955e81e8555dae5ddc2e9689939
lê van công <85329744+cong2001@users.noreply.github.com>
2021-06-04 13:47:13 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introTitle: 'MẤY BẠN à!',
+    introDesc: ` CHÀO TẤT CẢ CÁC BẠN XINH ĐẸP CỦA LỚP MÌNH
+    
+    
+    `,
+    btnIntro: 'HAHA',
+    title: 'Các bạn có biết không',
+    desc: 'mình hết tiền rồi :(( mỗi đứa cho mình 1tr nha  ',
+    btnYes: 'ok luôn. gửi số tài khoản đi',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    question:'2860101082001 MB BANK LÊ VĂN CÔNG',
+    btnReply: 'Gửi cho CÔNG',
+    reply: 'gửi liền nha',
+    mess: 'Công biết mấy bạn tốt mà',
+    messDesc: 'gửi đi nha. không gửi là chos',
+    btnAccept: 'ok luôn ',
+    messLink: 'http://cong21' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
